8b6c4a4de606bd5b62fe1f3627bc1ea108544cf9
ThanhDC <dev.thanhdc@gmail.com>
2026-06-09 21:28:47 +0700

    Initial Commit

---
+++ PasteHub/PasteHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteHubApp.swift
+//  PasteHub
+//
+//  Created by ThanhDC on 9/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PasteHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
